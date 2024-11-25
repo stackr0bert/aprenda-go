c336ba4ced78dbdce1d2613d4c4e36232485d393
r0bert <robertdsgabriel@gmail.com>
2024-11-25 00:49:21 -0300

    por hj é só ;D

--- modulos/cap4-fundamentos-programacao/nivel2/3/exerc3.go
+++ modulos/cap4-fundamentos-programacao/nivel2/3/exerc3.go
@@ -1,1 +1,18 @@
 // Crie constantes tipadas e não tipadas. Demostre seus valores --> olhar a documentação
+package main
+
+import "fmt"
+
+const (
+	a         = 1
+	b int16   = 10
+	c         = 50.2
+	d float32 = 11.2
+)
+
+func main() {
+	fmt.Printf("%v %T\n", a, a)
+	fmt.Printf("%v %T\n", b, b)
+	fmt.Printf("%v %T\n", c, c)
+	fmt.Printf("%v %T\n", d, d)
+}
